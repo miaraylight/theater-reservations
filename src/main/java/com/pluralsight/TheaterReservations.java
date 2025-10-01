a224ee24e18e503469a0d5869403ff6f374e5025
miaraylight <miaraylight@gmail.com>
2025-10-01 10:03:02 -0500

    main logic

--- src/main/java/com/pluralsight/TheaterReservations.java
+++ src/main/java/com/pluralsight/TheaterReservations.java
@@ -1,10 +1,13 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class TheaterReservations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter formatter;
 
         System.out.println("Please enter your name:");
         String name = scanner.nextLine();
@@ -15,10 +18,15 @@ public class TheaterReservations {
         System.out.println("How many tickets would you like?");
         int ticketAmount = scanner.nextInt();
 
-        System.out.printf("""
-                Name: %s,
-                Date: %s,
-                Number of tickets: %d
-                """, name, date, ticketAmount);
+        scanner.close();
+
+        String[] formattedName = name.split(" ");
+        String firstName = formattedName[0];
+        String lastName = formattedName[1];
+
+        formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        LocalDate formattedDate = LocalDate.parse(date, formatter);
+
+        System.out.printf("%d tickets reserved for %s under %s, %s", ticketAmount, formattedDate, lastName, firstName );
     }
 }
